49d9002009ccfc3b47fa6a994bc03d8d183facd0
Bry Cox <carl.cc@gmail.com>
2019-08-21 23:35:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/secret' => 'secrets#show'
   get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
